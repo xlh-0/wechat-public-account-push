a87f77edb6f1e1bc27afd156e26625478b284901
xlh <95605105+xlh-0@users.noreply.github.com>
2022-10-12 20:35:37 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -24,7 +24,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o3Mys6Qy-5JZoVIme_If1fomgU48',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'tCfsZGDdx-KD1zZZubINZliXPcBkxUWJliflSRvTrRI',
+      useTemplateId: 'VyOfyLIVWind0rMJEZrQq5JT_sAJK3ZIkrpB87EGHTQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-26',
       // 我们在一起已经有xxxx天了的配置
